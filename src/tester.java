eb895a1cc2ef8394649d3680f6e208814f237aef
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-20 12:27:14 +0200

    last

--- src/tester.java
+++ src/tester.java
@@ -2,7 +2,7 @@ package src;
 
 public class tester {
     public static void main(String[] args) {
-        System.out.print(src.fastaToString.reader("src/my_fasta.fasta"));
+        System.out.print(src.fastaToString.reader("my_fasta.fasta"));
     }
     
 }
