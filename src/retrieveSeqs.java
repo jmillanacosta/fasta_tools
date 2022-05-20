d253f3fd49af7996c6b3357ba435f805aea5cf53
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-20 12:25:31 +0200

    commit

---
+++ src/retrieveSeqs.java
@@ -0,0 +1,14 @@
+/**
+* Opens a string of fasta sequences and separates them into individual sequences 
+* @param  seqFasta  a string
+*/
+
+package src;
+
+public class retrieveSeqs {
+    static String[] retriever(String seqFasta){
+        String seqs[]  = {};            
+        return seqs;
+    }
+    
+}
---
+++ src/tester.java
@@ -0,0 +1,8 @@
+package src;
+
+public class tester {
+    public static void main(String[] args) {
+        System.out.print(src.fastaToString.reader("src/my_fasta.fasta"));
+    }
+    
+}
