bfbac7c94560dd6de583ae7787cd7c8a9bd12d5d
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-21 12:34:54 +0200

    first

---
+++ src/sequences.java
@@ -0,0 +1,45 @@
+// Create a List of entries
+
+package src;
+import java.util.*;
+
+public class sequences implements Iterable<sequence> {
+    int size = 0;
+    // Array to store entries added to this list
+    public Object elements[];
+    //add method
+    public void add(sequence seq){
+        elements[size++] = seq;
+    }
+    // Constructor
+    public sequences(String file){
+        String[][] entries = fastaToString.reader(file);
+        for (String[] entry : entries){
+            sequence myEntry = new sequence(entry);
+            this.add(myEntry);
+        }
+    }
+    // Makes class iterable
+    private final List<sequence> seqList = new ArrayList<sequence>();
+
+    @Override
+    public Iterator<sequence> iterator() {
+        return seqList.iterator();
+    }
+
+
+    // Method that gets the IDs of the entries
+    public String getIds(){
+        StringBuilder ids_build = new StringBuilder();
+        for (sequence seq : seqList){
+            ids_build.append(seq.seq);       
+        }
+        String ids = ids_build.toString();
+        return ids;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("h");
+        
+    }    
+}
