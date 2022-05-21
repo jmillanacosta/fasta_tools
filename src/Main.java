55e93181163835b044186d489a9413f4310100b5
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-21 13:12:54 +0200

    last

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 package src;
 
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        // Parse FASTA into String array
-        String[][] entries = src.fastaToString.reader("my_fasta.fasta");    
-        // Creates a list to construct all individual entries from the array of entries
-        List<sequence> sequences = new ArrayList<sequence>();
-        for (String[] entry : entries){
-            sequences.add(new sequence(entry));
-        }
-        
-
-
+        String file = "my_fasta.fasta";
+        sequences mySeqs = new sequences(file);
+        System.out.println(mySeqs.getIds());
     }
-    
 }
