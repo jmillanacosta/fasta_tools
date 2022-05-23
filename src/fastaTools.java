4a285dcfc49215e7b445903d4f5d30f3fe1c1f34
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-23 19:48:09 +0200

    working sort + remove

--- src/fastaTools.java
+++ src/fastaTools.java
@@ -1,8 +1,9 @@
 package src;
 
-public class Main {
+public class fastaTools {
     
     public static void main(String[] args) {   
+    // Argument string and formatting output
     String sep = "\n- - - - - - - - - - - - - - \n"; 
     String file = args[0].toString();
     System.out.println(sep + "\tFASTA TOOLS \nAnalyzing entries in " + file + sep);
@@ -10,7 +11,11 @@ public class Main {
     String option = args[1].toString();
     String p = args[2].toString();
     String outFile = args[3].toString();
-    System.out.println(file + "  " + option + "  " + p + "  " + outFile);
+    System.out.println(file + " | " + option + " | " + p + " | " + outFile);
+
+    // Select option 
+
+    // Removing an entry
     if (option.equals("r")){
         System.out.println("\nr - Remove entries from file.");
         System.out.println("The following entries match the pattern (" + p + ") and will be removed:");
@@ -19,7 +24,24 @@ public class Main {
         mySeqs.writeFasta(outFile);  
 
     }    
-        
+    // Sorting entries
+    if (option.equals("sa") || option.equals("sd")){
+        String order = "invalid";
+
+        if (option.equals("sa")){
+            order = "ascending";
+        }
+        if (option.equals("sd")){
+            order = "descending";
+        }
+
+        System.out.println(order);
+        System.out.println("\ns - Sort entries in " + order + " order");
+        mySeqs.sortLength(order);
+        System.out.println("\n");
+        mySeqs.writeFasta(outFile);
+    }
+       
     }
     
 }
