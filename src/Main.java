77d8002662bcb7a51dcad4ab9973103ebf4ec6c6
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-23 10:32:03 +0200

    fixed

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         String file = "my_fasta.fasta";
         sequences mySeqs = new sequences(file);
-        System.out.println(mySeqs.getSeqs());
+        System.out.println(mySeqs.getLength());
+        mySeqs.sortLength("ascending");
+        System.out.println("____________\n" +mySeqs.getLength());
+        mySeqs.sortLength("descending");
+        System.out.println("____________\n" +mySeqs.getLength());
+        
+        
     }
 }
