98e86690d1c7440ace5ab4d6b5d2876c726bc1ba
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-23 10:33:37 +0200

    fixed wrong commit

--- src/sequences.java
+++ src/sequences.java
@@ -1,4 +1,4 @@
-// Create a List of entries
+// Class is an iterable list of FASTA entries. Most program functionalities are methods here.
 
 package src;
 import java.util.*;
@@ -12,14 +12,25 @@ public class sequences implements Iterable<sequence> {
         elements[size++] = seq;
     }
     // Constructor
-    public sequences(String file){
-        String[][] entries = fastaToString.reader(file);
-        for (String[] entry : entries){
-            sequence myEntry = new sequence(entry);
-            seqList.add(myEntry);
+    public sequences(String file){ 
+
+    String[][] entries = fastaToString.reader(file);
+    List<String> seen = new ArrayList<String>();
+    for (String[] entry : entries){
+        sequence myEntry = new sequence(entry);
+        if (seen.contains(myEntry.id)){
+            System.out.println("Duplicate ignored");
+            
+        }else{
+            seqList.add(myEntry); 
+            seen.add(myEntry.id);
         }
+        
     }
-    // Makes class iterable
+ 
+    }
+
+    // Makes class an iterable list
     public final List<sequence> seqList = new ArrayList<sequence>();
 
     @Override
@@ -33,7 +44,7 @@ public class sequences implements Iterable<sequence> {
         StringBuilder ids_build = new StringBuilder();
         int i = 0;
         for (sequence seq : seqList){
-            ids_build.append("Sequence identifier " + i + ": " + seq.id + "\n");  
+            ids_build.append("Entry " + i + ": " + seq.id + "\n\n");  
             i++;     
         }
         String ids = ids_build.toString();
@@ -51,5 +62,53 @@ public class sequences implements Iterable<sequence> {
         String seqs = seqs_build.toString();
         return seqs;
     }
-}
 
+    // Method that gets the length of the sequences of the entries
+    public String getLength(){
+        // Creates a StringBuilder to store all lengths and formatted text
+        StringBuilder length_build = new StringBuilder();
+        // Loops through all sequence objects in List<sequence> and retrieves seq.length
+        int i = 0;
+        for (sequence seq : seqList){
+            length_build.append("Entry " + i + ": " + seq.id + " --> Length is " + seq.length + "\n\n");  
+            i++;     
+        }
+        // Convert StringBuilder to String
+        String length = length_build.toString();
+        return length;
+    }
+
+    // Method that browses a query ID and returns the match(es)
+    
+
+    // Method that sorts the sequences by length (ascending / descending)
+    public void sortLength(String order) {
+        // List of int to store lengths
+        List<Integer> lengths = new ArrayList<Integer>();
+        for (sequence seq : seqList){
+            lengths.add(seq.length);
+        }
+        // ascending or descending 
+        Collections.sort(lengths);
+        if (order == "descending"){
+            Collections.reverse(lengths);
+        }
+        // Creates temporary List<sequence> to store sequences sorted by length
+        List<sequence> newSeqList = new ArrayList<sequence>();
+        for (int i = 0; i < lengths.size(); i++){
+            for (sequence seq : seqList){
+                if (lengths.get(i) == seq.length){
+                    newSeqList.add(seq);
+                    //System.out.println(i + "__" + seq.id + "  " + seq.length + "  ");
+                }  
+            }
+        }  
+        // Sets the sequences from the temporary list to the seqList to sort all entries by length
+        for (int j = 0; j < newSeqList.size(); j++){
+            sequence newSeq = newSeqList.get(j);
+            seqList.set(j, newSeq);         
+        }    
+    }
+
+
+}
