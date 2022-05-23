efab0bf2761fc098f6d4f54c31f11d4dfddf4ff6
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-23 13:18:59 +0200

    last

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ package src;
 
 public class Main {
     
-    public static void main(String[] args) {   
+    
         String sep = "\n- - - - - - - - - - - - - - \n";    
 
         String file = "try3.fasta";
         System.out.println(sep + "\tFASTA TOOLS \nAnalyzing entries in " + file + sep);
-        sequences mySeqs = new sequences(file);
+        String sep = "\n- - - - - - - - - - - - - - \n";    
         mySeqs.writeFasta();        
-    }
-}
