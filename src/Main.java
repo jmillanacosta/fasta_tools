59dd2882119945b00a4000b47273bc086c15f5bb
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-23 15:45:34 +0200

    seqs not working

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,24 @@ import java.util.List;
 public class Main {
     
     public static void main(String[] args) {   
-        String sep = "\n- - - - - - - - - - - - - - \n";    
-
-        String file = "my_fasta.fasta";
+        String sep = "\n- - - - - - - - - - - - - - \n"; 
+        String file = args[0];
         System.out.println(sep + "\tFASTA TOOLS \nAnalyzing entries in " + file + sep);
-
         sequences mySeqs = new sequences(file);
-        List<sequence> removedSeqs = mySeqs.browse("dummy");
-        mySeqs.removeSeqs(removedSeqs);
-        mySeqs.writeFasta();
+        String option = args[1];
+        String p = args[2];
+        String outFile = args[3];
+        System.out.println(file + "  " + option + "  " + p + "  " + outFile);
+
+//if (option == "r"){
+//    System.out.print("\nr - Remove entries from file.");
+//    System.out.println("The following entries match the pattern (" + p + ") and will be removed:");
+//    List<sequence> removedSeqs = mySeqs.browse(p);
+//    mySeqs.removeSeqs(removedSeqs);
+//    mySeqs.writeFasta(outFile);  
+//
+//}    
+        
     }
+    
 }
