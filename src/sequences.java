0c477d30ae9dfc998193fbc5579688e8c7e02c01
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-23 10:51:31 +0200

    working

--- src/sequences.java
+++ src/sequences.java
@@ -51,7 +51,6 @@ public class sequences implements Iterable<sequence> {
         return ids;
     }
 
-        StringBuilder length_build = new StringBuilder();
     // Method that gets the seqs of the entries
     public String getSeqs(){
         StringBuilder seqs_build = new StringBuilder();
