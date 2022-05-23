286970b3d994e2c626aa6c6193c0a994d6a98a45
jmillanacosta <83466805+jmillanacosta@users.noreply.github.com>
2022-05-23 17:41:03 +0200

    working!

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,24 @@
 package src;
-import java.util.List;
 
 public class Main {
     
     public static void main(String[] args) {   
-        String sep = "\n- - - - - - - - - - - - - - \n"; 
-        String file = args[0];
-        System.out.println(sep + "\tFASTA TOOLS \nAnalyzing entries in " + file + sep);
-        sequences mySeqs = new sequences(file);
-        String option = args[1];
-        String p = args[2];
-        String outFile = args[3];
-        System.out.println(file + "  " + option + "  " + p + "  " + outFile);
+    String sep = "\n- - - - - - - - - - - - - - \n"; 
+    String file = args[0].toString();
+    System.out.println(sep + "\tFASTA TOOLS \nAnalyzing entries in " + file + sep);
+    sequences mySeqs = new sequences(file);
+    String option = args[1].toString();
+    String p = args[2].toString();
+    String outFile = args[3].toString();
+    System.out.println(file + "  " + option + "  " + p + "  " + outFile);
+    if (option.equals("r")){
+        System.out.println("\nr - Remove entries from file.");
+        System.out.println("The following entries match the pattern (" + p + ") and will be removed:");
+        mySeqs.removeSeqs(mySeqs.browse(p));
+        System.out.println("\n");
+        mySeqs.writeFasta(outFile);  
 
-//if (option == "r"){
-//    System.out.print("\nr - Remove entries from file.");
-//    System.out.println("The following entries match the pattern (" + p + ") and will be removed:");
-//    List<sequence> removedSeqs = mySeqs.browse(p);
-//    mySeqs.removeSeqs(removedSeqs);
-//    mySeqs.writeFasta(outFile);  
-//
-//}    
+    }    
         
     }
     
